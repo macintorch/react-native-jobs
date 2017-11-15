52130238483b113e667fd15e56e085ae4832a012
Ainor Syahrizal <macintorch@gmail.com>
2017-11-16 03:51:00 +0800

    add two more items in TabNavigator

--- App.js
+++ App.js
@@ -4,14 +4,24 @@ import { TabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 
 export default class App extends React.Component {
   render() {
 
     const MainNavigator= TabNavigator({
-      welcome: {screen: WelcomeScreen},
-      auth: {screen: AuthScreen},
+      Welcome: {screen: WelcomeScreen},
+      Auth: {screen: AuthScreen},
+        main: {
+          screen: TabNavigator({
+            map:{ screen: MapScreen},
+            deck: { screen: DeckScreen}
+          }, {
+            tabBarPosition: 'bottom'
+          })
+        }
       }, {
         tabBarPosition: 'bottom'
         });
