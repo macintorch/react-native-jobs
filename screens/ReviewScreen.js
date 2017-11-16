168998ec0f0b65c60379cb2f3beed1c17df6f984
Ainor Syahrizal <macintorch@gmail.com>
2017-11-16 13:58:52 +0800

    add slides

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text } from 'react-native';
+import {View, Text, Platform } from 'react-native';
 import {Button} from 'react-native-elements';
 
 class ReviewScreen extends Component {
@@ -8,8 +8,19 @@ class ReviewScreen extends Component {
 	static navigationOptions = ({navigation}) => {
 		return {
 			title: 'Review Jobs',
-			headerRight: <Button title="Settings" onPress={()=> { navigation.navigate('settings') }}/>
-		}
+			headerRight: (
+				<Button 
+					title="Settings" 
+					onPress={()=> { navigation.navigate('settings') }}
+					backgroundColor = "rgba(0,0,0,0)"
+					color = "rgba(0, 122, 255, 1)"
+					/>
+				),
+			style: {
+				// if plaform is android then return 24 else return 0
+				marginTop: Platform.OS === 'android' ? 24: 0
+			}
+		};
 		
 	}
 
