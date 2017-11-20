af47236ea10c2cebdf7f31f271ae810b316444d3
Ainor Syahrizal <macintorch@gmail.com>
2017-11-21 04:50:43 +0800

    add reset button in settings screen

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import {
-	LIKE_JOB
+	LIKE_JOB,
+	CLEAR_LIKED_JOBS
 } from '../actions/types';
 
 export default function(state = [], action) {
@@ -10,6 +11,10 @@ export default function(state = [], action) {
 			return _.uniqBy([
 				action.payload, ...state
 				],'jobkey');
+
+		case CLEAR_LIKED_JOBS:
+			return [];
+			
 		default:
 		return state;
 	}
